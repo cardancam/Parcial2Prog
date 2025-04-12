e9ce14c259d56cd48fe7a6e965c01b4d0887fa00
CarlosCamacho222 <cdcamacho30@gmail.com>
2025-04-11 19:57:40 -0500

    Se crea clase Arma

---
+++ Parcial2Prog/src/uniajc/Arma.java
@@ -0,0 +1,18 @@
+package uniajc;
+
+public class Arma {
+    String nombreArma;
+    int daño;
+
+    public Arma(String nombreArma, int daño) {
+        this.nombreArma = nombreArma;
+        this.daño = daño;
+    }
+    
+    public void atacarConArma(){
+        
+    }
+    public void getDañodicional(){
+        
+    }
+}
